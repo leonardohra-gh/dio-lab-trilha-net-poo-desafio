977c58bff069c54c8bbc9f82775a030c401775d8
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-09-29 19:06:41 -0300

    Corrigindo construtor

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -2,7 +2,7 @@ namespace DesafioPOO.Models
 {
     public class Iphone: Smartphone
     {
-        public Iphone(string nome, string modelo, string imei, int memoria) : base(nome, modelo, imei, memoria){}
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){}
 
         public override void InstalarAplicativo(string nomeApp)
         {
